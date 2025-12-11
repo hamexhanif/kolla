20925605795b5091373e01bedf70c481e823f16f
hamexhanif <hamexhanif@yahoo.com>
2025-12-12 00:09:20 +0100

    update UserRepository

--- backend/src/main/java/team5/prototype/repository/UserRepository.java
+++ backend/src/main/java/team5/prototype/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package team5.prototype.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import team5.prototype.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
